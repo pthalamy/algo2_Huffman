7ed7e086546ecabd798b63efb2b8044ccc7fac9c
Pierre Thalamy <pierre.thalamy@gmail.com>
2015-05-01 11:30:30 +0200

    Implement Calcul_Arbre and fix Affichage_Arbre

--- Huffman/arbre_huffman.adb
+++ Huffman/arbre_huffman.adb
@@ -3,114 +3,164 @@ use Ada.Text_IO, Comparaisons;
 
 package body Arbre_Huffman is
 
-	package Ma_File is new File_Priorite(Natural, Compare, Arbre);
-	use Ma_File;
+   package Ma_File is new File_Priorite(Natural, Compare, Arbre);
+   use Ma_File;
 
-	type TabFils is array(ChiffreBinaire) of Arbre ;
+   type TabFils is array(ChiffreBinaire) of Arbre ;
 
-	type Noeud(EstFeuille: Boolean) is record
-		case EstFeuille is
-			when True => Char : Character;
-			when False =>
-				Fils: TabFils;
-				-- on a: Fils(0) /= null and Fils(1) /= null
-		end case ;
-	end record;
+   type Noeud(EstFeuille: Boolean) is record
+      case EstFeuille is
+	 when True => Char : Character;
+	 when False =>
+	    Fils: TabFils;
+	    -- on a: Fils(0) /= null and Fils(1) /= null
+      end case ;
+   end record;
 
-	procedure Affiche_Arbre(A: Arbre) is
+   procedure Affiche_Arbre(A: Arbre) is
 
-	   type Trace is array(1..8) of ChiffreBinaire;
+      type Trace is array(1..8) of ChiffreBinaire;
 
-	   procedure Affiche_Rec(A : in Arbre;
-				 T : in out Trace;
-				 C : in out Natural) is
-	   begin
-	      if A.all.EstFeuille then
-		 for I in T'First..C loop
-		    Put (Integer'Image(Integer(T(I))));
-		 end loop;
+      procedure Affiche_Rec(A : in Arbre;
+			    T : in out Trace;
+			    C : in out Natural) is
+      begin
+	 if A.all.EstFeuille then
+	    for I in T'First..C loop
+	       Put (Integer'Image(Integer(T(I))));
+	    end loop;
 
-		 Put_Line ("-> " & A.Char);
-	      else
-		 C := C + 1;
+	    Put_Line (" -> " & A.Char);
+	 else
+	    C := C + 1;
 
-		 if A.Fils(0) /= null then
-		    T(C) := 0;
-		    Affiche_Rec (A.Fils(0), T, C);
-		 end if;
+	    if A.Fils(0) /= null then
+	       T(C) := 0;
+	       Affiche_Rec (A.Fils(0), T, C);
+	    end if;
 
-		 if A.Fils(1) /= null then
-		    T(C) := 1;
-		    Affiche_Rec (A.Fils(1), T, C);
-		 end if;
-	      end if;
+	    if A.Fils(1) /= null then
+	       T(C) := 1;
+	       Affiche_Rec (A.Fils(1), T, C);
+	    end if;
 
-	   end Affiche_Rec;
+	    C := C - 1;
+	 end if;
 
-	   T : Trace;
-	   C : Natural := 0;
-	begin
-	   if A = null then
-	      Put_Line ("Arbre inexistant.");
-	      return;
-	   end if;
+      end Affiche_Rec;
 
-	   Affiche_Rec (A, T, C);
+      T : Trace;
+      C : Natural := 0;
+   begin
+      if A = null then
+	 Put_Line ("Arbre inexistant.");
+	 return;
+      end if;
 
-	end Affiche_Arbre;
+      Affiche_Rec (A, T, C);
 
-	--algo principal : calcul l'arbre a partir des frequences
-	function Calcul_Arbre(Frequences : in Tableau_Ascii) return Arbre is
-		A : Arbre;
-	begin
-		return A; -- TODO
-	end Calcul_Arbre;
+   end Affiche_Arbre;
 
-	function Calcul_Dictionnaire(A : Arbre) return Dico is
-		D : Dico;
-	begin
-		-- TODO
-		return D;
-	end;
+   --algo principal : calcul l'arbre a partir des frequences
+   function Calcul_Arbre(Frequences : in Tableau_Ascii) return Arbre is
+      A : Arbre;
+      F : File;
+      Feuille : Arbre;
+      Taille : Natural := 0;
 
-	procedure Decodage_Code(Reste : in out Code;
-		Arbre_Huffman : Arbre;
-		Caractere : out Character) is
+      P : Natural := 0;
+      PSum : Natural := 0;
+      D : Arbre;
+      Statut : Boolean;
+      Fils_Gauche, Fils_Droite : Arbre;
+   begin
+      -- Mise en place de la file de priorité
+      for I in Frequences'Range loop
+	 if Frequences(I) > 0 then
+	    Taille := Taille + 1;
+	 end if;
+      end loop;
 
-		Position_Courante : Arbre;
-		Tmp,R : Natural;
-		Nouveau_Reste : Code;
-	begin
-		Position_Courante := Arbre_Huffman;
-		while not Position_Courante.EstFeuille loop
-			if Reste = null then
-				--chargement de l'octet suivant du fichier
-				Reste := new TabBits(1..8);
-				Caractere := Octet_Suivant;
-				Tmp := Character'Pos(Caractere);
-				for I in Reste'Range loop
-					R := Tmp mod 2;
-					Reste(Reste'Last + Reste'First - I) := R;
-					Tmp := Tmp / 2;
-				end loop;
-			end if;
-			Position_Courante := Position_Courante.Fils(Reste(1)) ;
-			if Reste'Last = 1 then
-				Liberer(Reste);
-				Reste := null;
-			else
-				-- TODO : modifier cette procedure
-				-- pour eviter de faire a chaque iteration
-				-- une allocation + 1 liberation
-				Nouveau_Reste := new TabBits(1..(Reste'Last - 1));
-				for I in Nouveau_Reste'Range loop
-					Nouveau_Reste(I) := Reste(I+1);
-				end loop;
-				Liberer(Reste);
-				Reste := Nouveau_Reste;
-			end if;
-		end loop;
-		Caractere := Position_Courante.Char;
-	end;
+      Put_Line ("Taille:" & Integer'Image(Taille));
+
+      -- TODO: FIND CLEANER WAY TO INIT HEAP
+      F := Nouvelle_File (256);
+
+      for I in Frequences'Range loop
+	 if Frequences(I) > 0 then
+	    Feuille := new Noeud'(EstFeuille => True, Char => I);
+	    Insertion(F, Frequences(I), Feuille);
+	 end if;
+      end loop;
+
+      loop
+	 Meilleur (F, P, D, Statut);
+	 exit when not Statut;
+	 Put_Line (Integer'Image(P));
+	 PSum := PSum + P;
+	 Fils_Gauche := D;
+	 Suppression (F);
+
+	 Meilleur (F, P, D, Statut);
+	 exit when not Statut;
+	 PSum := PSum + P;
+	 Fils_Droite := D;
+	 Suppression (F);
+
+	 A := new Noeud'(EstFeuille => False,
+			 Fils => (Fils_Gauche, Fils_Droite));
+	 Insertion(F, PSum, A);
+	 PSum := 0;
+      end loop;
+
+      return A;
+   end Calcul_Arbre;
+
+   function Calcul_Dictionnaire(A : Arbre) return Dico is
+      D : Dico;
+   begin
+      -- TODO
+      return D;
+   end;
+
+   procedure Decodage_Code(Reste : in out Code;
+			   Arbre_Huffman : Arbre;
+			   Caractere : out Character) is
+
+      Position_Courante : Arbre;
+      Tmp,R : Natural;
+      Nouveau_Reste : Code;
+   begin
+      Position_Courante := Arbre_Huffman;
+      while not Position_Courante.EstFeuille loop
+	 if Reste = null then
+	    --chargement de l'octet suivant du fichier
+	    Reste := new TabBits(1..8);
+	    Caractere := Octet_Suivant;
+	    Tmp := Character'Pos(Caractere);
+	    for I in Reste'Range loop
+	       R := Tmp mod 2;
+	       Reste(Reste'Last + Reste'First - I) := R;
+	       Tmp := Tmp / 2;
+	    end loop;
+	 end if;
+	 Position_Courante := Position_Courante.Fils(Reste(1)) ;
+	 if Reste'Last = 1 then
+	    Liberer(Reste);
+	    Reste := null;
+	 else
+	    -- TODO : modifier cette procedure
+	    -- pour eviter de faire a chaque iteration
+	    -- une allocation + 1 liberation
+	    Nouveau_Reste := new TabBits(1..(Reste'Last - 1));
+	    for I in Nouveau_Reste'Range loop
+	       Nouveau_Reste(I) := Reste(I+1);
+	    end loop;
+	    Liberer(Reste);
+	    Reste := Nouveau_Reste;
+	 end if;
+      end loop;
+      Caractere := Position_Courante.Char;
+   end;
 
 end;
